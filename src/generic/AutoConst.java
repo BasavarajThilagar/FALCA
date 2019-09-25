662e67319497403ae76268a9acfb1098525b7082
Basavaraj Thilagar <raj.thilagar@gmail.com>
2019-09-25 10:41:06 +0530

    change

--- src/generic/AutoConst.java
+++ src/generic/AutoConst.java
@@ -3,7 +3,12 @@ package generic;
 public interface AutoConst 
 {
 	String CHROME_KEY="webdriver.chrome.driver";
-	String CHROME_VALUE="./driver/chromedriver";
+	//Server
+	//String CHROME_VALUE="./driver/chromedriver";
+	
+	// Windows base
+	String CHROME_VALUE="./driver/chromedriver.exe";
+	
 	String FARMERAPK="./data/farmer.apk";
 	String FIELDAPK="./data/field.apk";
 	String CONFIG_PATH="./data/config.properties";
