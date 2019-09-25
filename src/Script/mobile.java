b427103c15ba123b989d61620ad673bcbeab0689
Basavaraj Thilagar <raj.thilagar@gmail.com>
2019-09-25 10:19:16 +0530

    change

--- src/Script/mobile.java
+++ src/Script/mobile.java
@@ -18,12 +18,14 @@ public class mobile extends WebBase
 	@Test(priority=1)
 	public void FarmerRegistration() throws InterruptedException, MalformedURLException
 	{
+		System.out.println("Entered inside mobile class");
 			File apk=new File(FARMERAPK);
 			DesiredCapabilities cap = new DesiredCapabilities();
 			cap.setCapability(MobileCapabilityType.DEVICE_NAME, "FALCA-MobileTest");
 			cap.setCapability(MobileCapabilityType.APP, apk.getAbsolutePath());
 			cap.setCapability("noReset", true);
 			AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),cap);
+			System.out.println("Mobile launched");
 			Thread.sleep(15000);
 			String a = driver.getDeviceTime();
 			System.out.println(a);
