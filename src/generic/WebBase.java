8eef9daa73a6ab22508346978842e0257c69cd0a
Sudeep Desai <sudeepdesai123@gmail.com>
2020-05-12 15:16:54 +0530

    new

--- src/generic/WebBase.java
+++ src/generic/WebBase.java
@@ -15,9 +15,8 @@ public class WebBase implements AutoConst
 	static
 	{
 		//System.setProperty(CHROME_KEY,"/usr/bin/chromedriver");
-		System.setProperty(CHROME_KEY,"/home/osboxes/Desktop/chromedriver");
 		
-		//System.setProperty(CHROME_KEY,CHROME_VALUE);
+		System.setProperty(CHROME_KEY,CHROME_VALUE);
 	}
 	public WebDriver driver;
 	
