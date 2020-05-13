e526548b183d802901a2957c8fabce288ad41872
Sudeep Desai <sudeepdesai123@gmail.com>
2020-05-13 09:40:40 +0530

    New code

--- src/Script/DemoConsumer.java
+++ src/Script/DemoConsumer.java
@@ -4,9 +4,8 @@ import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-//import POM.ConsumerLoginPage;
-//import POM.googleLoginPage;
-//import generic.Lib;
+import POM.ConsumerLoginPage;
+import generic.Lib;
 import generic.WebBase;
 
 public class DemoConsumer extends WebBase
@@ -16,28 +15,23 @@ public class DemoConsumer extends WebBase
 	{
 		try
 		{
+			Reporter.log("Browser opened");
+			
+			//driver.get("https://falca.in/login");
+			
+			String url = Lib.getProperty(CONFIG_PATH, "CosumerUrl");
+			driver.get(url);
 			Reporter.log("Inside login page");
-			
-			driver.get("https://falca.in/login");
-			
-			
-			
-			/*String url = Lib.getProperty(CONFIG_PATH, "url");
-			driver.get(url);
-			Thread.sleep(3000);
-			googleLoginPage google=new googleLoginPage(driver);
-			google.gbox();*/
-			/*Reporter.log("Inside login page");
-			String url = Lib.getProperty(CONFIG_PATH,"CosumerUrl");
-			driver.get(url);
+			Thread.sleep(6000);
 			ConsumerLoginPage login= new ConsumerLoginPage(driver);
 			String un = Lib.getProperty(CONFIG_PATH, "ConsumerUN");
 			login.SendUserName(un);
 			String pwd = Lib.getProperty(CONFIG_PATH, "ConsumerPwd");
 			login.SendPassword(pwd);
 			login.ClickLoginBtn();
-			Thread.sleep(8000);*/
+			Thread.sleep(8000);
 			Reporter.log("Admin loged-in Successfully.");
+			
 		}catch(Exception e)
 		{
 		Reporter.log(e.getMessage(),true);
